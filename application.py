5417216f837d6cbe9686d02cf450cdba695ecf10
Hammad Bin Sajjad <hammadbinsajjad@outlook.com>
2022-01-01 20:15:03 +0500

    Initial app running

---
+++ application.py
@@ -0,0 +1,22 @@
+# Importing libraries and custom classes/functions
+from flask import Flask, redirect, request, render_template, session, jsonify, flash
+from database import db
+
+# Creating and configuring flask app and SQLALchemy
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
+# For Linux below statement will work
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.app = app
+db.init_app(app)
+
+
+# App routes
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        return redirect("/")
